a7d32ffafbd8d54f376542769d5118f8191fa7cc
Ganesh Kannan <ganeshkannan995gk@gmail.com>
2025-08-22 14:40:38 +0530

    feat: Add controller and route for register

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,4 @@ Route::get('/', function () {
 });
 
 Route::get('/register', [RegisteredUserController::class, 'create']);
-Route::get('/register', [RegisteredUserController::class, 'store']);
-
-Route::get('/login', [SessionController::class, 'create']);
-Route::get('/login', [SessionController::class, 'store']);
-Route::get('/logout', [SessionController::class, 'destroy']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
